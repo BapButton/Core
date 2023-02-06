72e922316458da197af1f6e9123826d8e3764767
Nick Gelotte <nick.gelotte@rainforgrowth.com>
2023-02-05 23:23:22 -0800

    Audio Almost works

--- BAP.WebAudioPlayer/Components/AudioPlayer.razor.cs
+++ BAP.WebAudioPlayer/Components/AudioPlayer.razor.cs
@@ -12,101 +12,105 @@ using System.Threading.Tasks;
 
 namespace BAP.WebAudioPlayer.Components
 {
-    [TopMenu()]
-    public partial class AudioPlayer : ComponentBase, IDisposable
-    {
-        [Inject]
-        ILogger<AudioPlayer> _logger { get; set; } = default!;
-        [Inject]
-        IMemoryCache memoryCache { get; set; } = default!;
-        [Inject]
-        IJSRuntime js { get; set; } = default!;
-        [Inject]
-        ISubscriber<PlayAudioMessage> AudioMessagePipe { get; set; } = default!;
-        IDisposable Subscriptions { get; set; } = default!;
-        public bool VolumeMuted { get; set; }
-        protected override void OnInitialized()
-        {
-            var bag = DisposableBag.CreateBuilder();
-            AudioMessagePipe.Subscribe(async (x) => await PlayNextAudio(x)).AddTo(bag);
-            Subscriptions = bag.Build();
-            base.OnInitialized();
-        }
+	[TopMenu()]
+	public partial class AudioPlayer : ComponentBase, IDisposable
+	{
+		[Inject]
+		ILogger<AudioPlayer> _logger { get; set; } = default!;
+		[Inject]
+		IMemoryCache memoryCache { get; set; } = default!;
+		[Inject]
+		IJSRuntime js { get; set; } = default!;
+		[Inject]
+		ISubscriber<PlayAudioMessage> AudioMessagePipe { get; set; } = default!;
+		IDisposable Subscriptions { get; set; } = default!;
+		public bool VolumeMuted { get; set; }
+		protected override void OnInitialized()
+		{
+			var bag = DisposableBag.CreateBuilder();
+			AudioMessagePipe.Subscribe(async (x) => await PlayNextAudio(x)).AddTo(bag);
+			Subscriptions = bag.Build();
+			base.OnInitialized();
+		}
+		protected override async Task OnAfterRenderAsync(bool firstRender)
+		{
+			var module = await js.InvokeAsync<IJSObjectReference>(
+				"import", "./_content/BAP.WebAudioPlayer/Components/AudioPlayer.razor.js");
+		}
+		async Task PlayNextAudio(PlayAudioMessage e)
+		{
+			try
+			{
+				if (e.StopAllPlayingAudio)
+				{
+					await js.InvokeVoidAsync("StopAllAudio");
+				}
+				else if (e.ClearAllCachedAudio)
+				{
+					await js.InvokeVoidAsync("ClearAudio");
+				}
+				string fileNameToPlay = e.FullPathToAudioFileWithFileName;
+				if (!string.IsNullOrEmpty(e.FullPathToAudioFileWithFileName))
+				{
 
-        async Task PlayNextAudio(PlayAudioMessage e)
-        {
-            try
-            {
-                if (e.StopAllPlayingAudio)
-                {
-                    await js.InvokeVoidAsync("StopAllAudio");
-                }
-                else if (e.ClearAllCachedAudio)
-                {
-                    await js.InvokeVoidAsync("ClearAudio");
-                }
-                string fileNameToPlay = e.FullPathToAudioFileWithFileName;
-                if (!string.IsNullOrEmpty(e.FullPathToAudioFileWithFileName))
-                {
+					if (!e.FullPathToAudioFileWithFileName.StartsWith("/api"))
+					{
+						string justTheFilename = memoryCache.Get<string>(e.FullPathToAudioFileWithFileName) ?? "";
+						if (string.IsNullOrEmpty(justTheFilename))
+						{
+							string extension = Path.GetExtension(e.FullPathToAudioFileWithFileName);
+							string guid = Guid.NewGuid().ToString();
+							justTheFilename = $"{guid}{extension}";
+							memoryCache.Set(e.FullPathToAudioFileWithFileName, justTheFilename, TimeSpan.FromMinutes(60));
+						}
+						memoryCache.Set(justTheFilename, e.FullPathToAudioFileWithFileName, TimeSpan.FromMinutes(60));
+						fileNameToPlay = $"/api/audio/{justTheFilename}";
+					}
 
-                    if (!e.FullPathToAudioFileWithFileName.StartsWith("/api"))
-                    {
-                        string justTheFilename = memoryCache.Get<string>(e.FullPathToAudioFileWithFileName) ?? "";
-                        if(string.IsNullOrEmpty(justTheFilename))
-                        {
-                            string extension = Path.GetExtension(e.FullPathToAudioFileWithFileName);
-                            string guid = Guid.NewGuid().ToString();
-                            justTheFilename = $"{guid}{extension}";
-                            memoryCache.Set(e.FullPathToAudioFileWithFileName, justTheFilename, TimeSpan.FromMinutes(60));
-                        }
-                        memoryCache.Set(justTheFilename, e.FullPathToAudioFileWithFileName, TimeSpan.FromMinutes(60));
-                        fileNameToPlay = $"/api/audio/{justTheFilename}";
-                    }
-
-                }
-                if (!string.IsNullOrEmpty(fileNameToPlay))
-                {
-                    try
-                    {
-                        if (!string.IsNullOrEmpty(fileNameToPlay) && !VolumeMuted)
-                        {
-                            await js.InvokeVoidAsync("PlayAudioFile", fileNameToPlay);
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        _logger.LogError(ex.Message, $"Problem playing the sound -{ex.Message}");
-                    }
-                }
-
-
-            }
-            catch (Exception ex)
-            {
-
-                _logger.LogError(ex.Message, $"Problem playing sound with error -{ex.Message}");
-            }
+				}
+				if (!string.IsNullOrEmpty(fileNameToPlay))
+				{
+					try
+					{
+						if (!string.IsNullOrEmpty(fileNameToPlay) && !VolumeMuted)
+						{
+							await js.InvokeVoidAsync("PlayAudioFile", fileNameToPlay);
+						}
+					}
+					catch (Exception ex)
+					{
+						_logger.LogError(ex.Message, $"Problem playing the sound -{ex.Message}");
+					}
+				}
 
 
+			}
+			catch (Exception ex)
+			{
+
+				_logger.LogError(ex.Message, $"Problem playing sound with error -{ex.Message}");
+			}
 
 
-        }
 
-        async void ToggleAudio(MouseEventArgs e)
-        {
-            VolumeMuted = !VolumeMuted;
-            await InvokeAsync(() =>
-            {
-                StateHasChanged();
-            });
-        }
 
-        public void Dispose()
-        {
-            if (Subscriptions != null)
-            {
-                Subscriptions.Dispose();
-            }
-        }
-    }
+		}
+
+		async void ToggleAudio(MouseEventArgs e)
+		{
+			VolumeMuted = !VolumeMuted;
+			await InvokeAsync(() =>
+			{
+				StateHasChanged();
+			});
+		}
+
+		public void Dispose()
+		{
+			if (Subscriptions != null)
+			{
+				Subscriptions.Dispose();
+			}
+		}
+	}
 }
---
+++ BAP.WebAudioPlayer/Components/AudioPlayer.razor.js
@@ -0,0 +1,53 @@
+﻿window.PlayAudioFile = function (src) {
+	var aplayerName = src.replaceAll('/', '_').replaceAll('.', '_');
+	var audio = document.getElementById(aplayerName);
+	if (audio === undefined || audio === null) {
+		var audio = document.createElement('audio');
+		audio.id = aplayerName;
+		audio.src = src;
+		audio.type = 'audio/mpeg';
+		document.body.appendChild(audio);
+	}
+	console.log("Audio Stats");
+	console.log(audio.paused);
+	console.log(audio.currentTime);
+	console.log(audio.ended);
+	if (!audio.paused && audio.currentTime > 0 && !audio.ended) {
+		console.log("Reset current Time");
+		audio.currentTime = 0;
+	}
+	if (audio.paused && audio.currentTime > 0) {
+		console.log("Reset current Time because it is paused");
+		audio.currentTime = 0;
+		audio.play();
+	}
+	else {
+		audio.play();
+	}
+
+}
+window.StopAllAudio = function () {
+	var element = document.getElementsByTagName("audio");
+	for (index = element.length - 1; index >= 0; index--) {
+		if (element.id !== 'audioPlayer') {
+			var audio = element[index];
+			if (!audio.paused && audio.currentTime > 0 && !audio.ended) {
+				audio.pause();
+			}
+		}
+
+	}
+
+}
+window.ClearAudio = function () {
+	var element = document.getElementsByTagName("audio");
+	let index = 0;
+
+	for (index = element.length - 1; index >= 0; index--) {
+		if (element.id !== 'audioPlayer') {
+			element[index].parentNode.removeChild(element[index]);
+		}
+
+	}
+
+}
