53541f03eb2fd7d9d1a26e8e363a98d58529da68
Nick Gelotte <nick.gelotte@rainforgrowth.com>
2023-03-01 16:13:29 -0800

    Message pack should not be required for Bap.Testers

--- BAP.TestUtilities/Components/MockButton.razor.cs
+++ BAP.TestUtilities/Components/MockButton.razor.cs
@@ -1,9 +1,4 @@
-﻿using MessagePack.Resolvers;
-using MessagePipe;
-using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Components;
 
 namespace BAP.TestUtilities
 {
